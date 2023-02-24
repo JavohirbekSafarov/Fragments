d0bde0df53bfec7bf75cdd4c0948ec4e7ea9dc68
Javohirbek <safarovjavohirbek3@gmail.com>
2023-02-24 11:45:17 +0500

    Initial commit

---
+++ app/src/main/java/com/javohirbekcoder/fragments/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.javohirbekcoder.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.javohirbekcoder.fragments.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        laodFragment(Home_Fragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    laodFragment(Home_Fragment())
+                    true
+                }
+                R.id.seach -> {
+                    laodFragment(Search_Fragment())
+                    true
+                }
+                R.id.add -> {
+                    laodFragment(Add_Fragment())
+                    true
+                }
+                R.id.like -> {
+                    laodFragment(Like_Fragment())
+                    true
+                }
+                R.id.account -> {
+                    laodFragment(AccountFragment())
+                    true
+                }
+                else -> {
+                    laodFragment(Home_Fragment())
+                    true
+                }
+            }
+        }
+    }
+
+    private fun laodFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
+    }
+}
+
